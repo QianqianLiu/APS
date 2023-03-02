f92cfca15453757edbcf39bd275c4fefc6398b42
kboot2022 <108757520+kboot2022@users.noreply.github.com>
2023-03-02 17:19:37 -0500

    Update stampede_comp.py

--- stampede_comp.py
+++ stampede_comp.py
@@ -1,3 +1,4 @@
 from pylibs import *
 
 # Check the atmospheric forcing (wind - sflux in netcdf format - either interpolate multiple stations for comparison, or pick the closest station) compared to observations - same method as comparison of results 
+# The SCHISM input files are located in sflux as .nc format, pull points from the grid using pylibs (ReadNC: read netcdf file content as zdata format and C.VINFO) compare to the NDBC station data, starting around the inlets. 
