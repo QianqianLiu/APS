443cacf2d352fecc803031c44c229f35db7b4850
kboot2022 <kboot42@gmail.com>
2023-03-31 12:34:50 -0400

    add figure

--- Calibration/Compare_NOAA_Elev_Year.py
+++ Calibration/Compare_NOAA_Elev_Year.py
@@ -10,7 +10,7 @@ xts,xls=xts[::60],xls[::60]; xls[0]=xls[0]#+', 2018'# orig 2, not 60
 
 # Load model output data from station points; change address accordingly; this information needs to be extracted (pextract + stations.bp) before running this code!
 
-mod = loadz("/home/liuq/Analysis/APS/RUN02a/mod_at_noaa_stations.npz")
+mod = loadz("/home/bootk/Analysis/mod_at_noaa_stations.npz")
 stations= [8658120, 8658163, 8656483, 8654467, 8652587, 8651370]; # NDBC Station numbers used for comparison
 
 # String of station names, same order as numbers, for figure labelling later
@@ -45,9 +45,9 @@ for st,sta in enumerate(stations):
     setp(gca(),xticks=xts, xticklabels=xls, xlim=[datenum(2019, 1, 1), datenum(2019, 12, 31)],ylim=[-.4, .6])
     title("Station "+str(station_names[st]))
     plt.tight_layout()
-    plt.suptitle('Elevation Comparison at NOAA Stations - 2019', fontsize = 20)
+    plt.suptitle('Elevation Comparison at NOAA Stations - 2019 - RUN02b', fontsize = 20)
 
 show(block=False)
 
-savefig('Compare_Elev_2019.png')
+savefig('Compare_Elev_2019_RUN02b.png')
 
