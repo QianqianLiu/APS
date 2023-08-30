1e0a4c655f00d56e6fcd07a8a6eb9947f8c4382c
liuq <liuqianqian0622@gmail.com>
2023-08-30 12:46:03 -0400

    test on dell

--- grid_optimization.py
+++ grid_optimization.py
@@ -6,12 +6,12 @@ from pylib import *
 ## download the files in tif format
 ## use the following command to convert the files to npz format used by zhengui:
 
-#convert_dem_format('ncei19_nxxxx.tif','test.npz',fmt=1)
+convert_dem_format('ncei19_nxxxx.tif','test.npz',fmt=1)
 
 #Then use the commands below to convert the bathymetry files to contours
-#test=loadz('test.npz')
-#levels=[-5,-4,-3,-2,-1,0]
-#compute_contour(test.lon,test.lat,test.elev,levels,fname='contours')
+test=loadz('test.npz')
+levels=[-5,-4,-3,-2,-1,0]
+compute_contour(test.lon,test.lat,test.elev,levels,fname='contours')
 #the files will be saved as contours_m5/4/3/2/1/0.shx/dbf/prj/shp files.
 
 #can combine multiple shapefiles
