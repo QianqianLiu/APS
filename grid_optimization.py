e62296e4f428cc6cdab87a9c279f0081e6c37414
liuq <liuqianqian0622@gmail.com>
2023-09-01 12:18:50 -0400

    update grid_optimization.py

--- grid_optimization.py
+++ grid_optimization.py
@@ -5,22 +5,35 @@ from pylib import *
 ## https://coast.noaa.gov/htdata/raster2/elevation/NCEI_ninth_Topobathy_2014_8483/
 ## download the files in tif format
 ## use the following command to convert the files to npz format used by zhengui:
+# Then use the commands below to convert the bathymetry files to contours
 
 import glob
 tiffiles = glob.glob('../NC_DEMs/*.tif')
 
-for file in tiffiles:
-    convert_dem_format(file,'oa.npz',fmt=1)
-    test=loadz('oa.npz')
-    levels=[]
+for of,file in enumerate(tiffiles):
+    convert_dem_format(file,'../NC_DEMs/oa.npz',fmt=1)
+    test=loadz('../NC_DEMs/oa.npz')
+    levels=[-6, -5, -4, -3, -2, -1, 0]
+    fname='../NC_DEMs/contours/contours'+str(of)
+    compute_contour(test.lon,test.lat,test.elev,levels,fname=fname)
 
-convert_dem_format('ncei19_nxxxx.tif','test.npz',fmt=1)
+import pandas as pd
+import geopandas as gpd
 
-#Then use the commands below to convert the bathymetry files to contours
-test=loadz('test.npz')
-levels=[-5,-4,-3,-2,-1,0]
-compute_contour(test.lon,test.lat,test.elev,levels,fname='contours')
-#the files will be saved as contours_m5/4/3/2/1/0.shx/dbf/prj/shp files.
 
+#cfiles = glob.glob('../NC_DEMs/contours/contour*_m0.shp')
+
+for le in levels:
+    cfiles = glob.glob('../NC_DEMs/contours/contour*_m{}.shp'.format(abs(le)))
+    for i,cfile in enumerate(cfiles):
+        gdfi = gpd.read_file(cfile)
+        if i==0:
+            gdf=gdfi
+        else:
+            gdf = gpd.GeoDataFrame(pd.concat([gdf,gdfi]))
+
+    gdf.to_file('../NC_DEMs/contour_m{}.shp'.format(abs(le)))
+
+#the files will be saved as contours0/1/2/3/4/5/6_m5/4/3/2/1/0.shx/dbf/prj/shp files.
 #can combine multiple shapefiles
 
