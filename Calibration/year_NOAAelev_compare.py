ec0a1ea9fecab3d82288a2d1fa7a164c6726101e
kboot2022 <kboot42@gmail.com>
2023-12-18 15:45:13 -0500

    plotting edits

--- Calibration/year_NOAAelev_compare.py
+++ Calibration/year_NOAAelev_compare.py
@@ -20,59 +20,53 @@ station_names= ['WLON7, Wilmington, NC','JMPN7 - Wrightsville Beach, NC','BFTN7 
 
 # Load obs station data
 obs = loadz("/home/bootk/git_liu/APS/Obs/extract_noaa_sta_msl/noaa_elev_navd.npz")
-
 #Create figure
 figure(figsize=[15, 6])
-
 # Set the x axis "times" based on one year (will display ad day/month as per str.
 xts,xls=get_xtick(fmt=2,xts=[datenum(2019,1,1),datenum(2019,12,31)],str='%b')
-
 # Only show values/x axis points every 60 rows/2 months?? 
 xts,xls=xts[::60],xls[::60]; 
 
-# This line supposedly redundant according to ChatGPT
-#xls[0]=xls[0]
+# Plot comparison for  6 total stations
 
-# Plot comparison
-# 6 total stations
+for st, sta in enumerate(stations):
+    subplot(2, 3, st + 1)
 
-for st,sta in enumerate(stations): 
-    subplot(2,3,st+1)
-	
-    # define station observations from 01/01 - 12/31, 2019
-    ind = (obs.station == sta) * ( obs.time >= datenum(2019, 1, 1) ) * (obs.time < datenum(2019, 12, 31))
+    # Define station observations from 01/01 - 12/31, 2019
+   
+    ind_19 = (obs.station == sta) & (obs.time >= datenum(2019, 1, 1)) & (obs.time < datenum(2019, 12, 31))
+    oyi_19 = obs.elev[ind_19]
+    oyi_19 = oyi_19 - oyi_19.mean()  # Assign the elevation observations for 19
 
-    oyi=obs.elev[ind]; oyi=oyi-oyi.mean() # Assign the elevation observations 
+    foyi_19 = lpfilt(oyi_19, 1 / 24, 0.2)  # Low-pass filter on 19 observation data
 
-##### Error here for lpfilt
-    foyi = lpfilt(oyi,1/24,0.2) # low pass filter on observation data -- 1/24 is hourly converted to daily
-    
-    # Alternative smooth filter - not used
-    #soyi = smooth(oyi, 24)
-    
-    # plot filtered observations time (x) versus elev(y) as red line
-    
-    # plot(obs.time[ind], oyi, "r-") 
-    plot(obs.time[ind], foyi, "b-")
-    #plot(obs.time[ind], soyi, "r-") # Plot smoothed obs - green line
-    
-    ######################## Plot model outputs (extracted previously)##############
-    
-    #myi=mod.elev[st,:]; myi=myi-myi.mean() # define model elevations
-    #fmyi = lpfilt(myi,1/24,0.2) # Low pass filter on model data
-    #smyi = smooth(myi, 24) # smooth filter on model data
-    #plot(mod.time + datenum(2019, 1, 1) + 00 / 24, fmyi,"b-") # plot model time (x) every 24 hours (??) with filtered elevation (y) as blue line
-    
-# Observations always use UTC, but model used local time, however we changed model to UTC so there is no longer a 4 hour lag
+    # Plot 19 data
+    plot(obs.time[ind_19], foyi_19, "b-", label='2019')
 
-#plot(mod.time + datenum(2019, 1, 1) + 00 / 24, smyi,"g-") # plot model time (x) every 24 hours (??) with smoothed elevation (y) as green line
-    setp(gca(),xticks=xts, xticklabels=xls, xlim=[datenum(2019, 1, 1), datenum(2019, 12, 31)],ylim=[-.4, .6])
+    # Define station observations from 01/01 - 12/31, 2002
+    
+    ind_02 = (obs.station == sta) & (obs.time >= datenum(2002, 1, 1)) & (obs.time < datenum(2002, 12, 31))
+    oyi_02 = obs.elev[ind_02]
+    
+    if oyi_02.size == 0:
+        print(f"No station data for 2002 at station {sta}")
+    else:
+        oyi_02 = oyi_02 - oyi_02.mean()  # Assign the elevation observations for 2002
+        foyi_02 = lpfilt(oyi_02, 1 / 24, 0.2)  # Low-pass filter on 2002 observation data
+        plot(obs.time[ind_02], oyi_02, "r-", label='2002 Raw')
+        #plot(obs.time[ind_02], foyi_02, "r-", label='2002')
+        print('Size of oyi_02:', oyi_02.size)
+
+    setp(gca(), xticks=xts, xticklabels=xls, xlim=[datenum(2019, 1, 1), datenum(2019, 12, 31)], ylim=[-0.4, 0.6])
     plt.xticks(rotation=315)
-    title("Station "+str(station_names[st]))
+    title("Station " + str(station_names[st]))
     plt.tight_layout()
+    plt.legend()
     plt.suptitle(f'Elevation Comparison at NOAA Stations {years[0]} and {years[1]}', fontsize=20)
 
 show(block=False)
+
+
 
 #<<<<<<< HEAD
 #savefig('Compare_Elev_lpfilt_2019.png')
