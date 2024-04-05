2abb494aa134e2884ef246c8daa0fe08473c2afb
kboot2022 <kboot42@gmail.com>
2024-04-05 12:55:10 -0400

    matlab and shell script add

--- matlab/station_plotting_codes/NDBC_Stations_Plot.m
+++ matlab/station_plotting_codes/NDBC_Stations_Plot.m
@@ -21,7 +21,7 @@ depth=griddata(dep(:,2),dep(:,3),dep(:,4),long,lati,'natural');
 % hold on
 
 %% Bathymetry - Zoomed in scale for focus on APS study area with color
-
+figure;
 patch('Faces',nv,'Vertices',[long lati],...
 'FaceVertexCData',depth,'FaceColor','interp','EdgeColor','none');
 caxis([0 50]); %set colormap limits to depth range
@@ -29,31 +29,41 @@ caxis([0 50]); %set colormap limits to depth range
 colormap(); %jet
 colorbar;
 c = colorbar;
-c.Label.String = 'Water Depth (meter)';
+c.Label.String = 'Water Depth (meters)';
 hold on;
-
 axis equal; axis([-78 -74.8 33.5 36.5]);
 set(gca,'xtick',[-78:1:-74],'xticklabel',num2str([78:-1:74]'),'fontsize',12);
 set(gca,'ytick',[33:1:36],'yticklabel',num2str([33:1:36]'),'fontsize',12);
-box('on'); grid on %turn box and grid on/off as needed
-% % set(gca,'visible','off') %Turns off axis and tickmarks
-
-xlabel('Longitude (^oW)','fontsize',12);
-ylabel('Latitude (^oN)','fontsize',12);
+box('on'); 
 hold on
-%% Plot Stations
-
-title('Monitoring Station Locations')
 
 % Used for comparison right now
-%stations= [8658120, 8658163, 8656483, 8654467, 8652587, 8651370];
-%station_names= ['WLON7, Wilmington, NC','JMPN7 - Wrightsville Beach, NC','BFTN7 Beaufort, NC','HCGN7 - USCG Station Hatteras, NC','ORIN7 - Oregon Inlet Marina, NC','DUKN7 - Duck Pier, NC'];
+stations = {'8658120', '8658163', '8656483', '8654467', '8652587', '8651370'};
+station_names= {'WLON7 - Wilmington, NC','JMPN7 - Wrightsville Beach, NC','BFTN7 - Beaufort, NC','HCGN7 - USCG Station Hatteras, NC','ORIN7 - Oregon Inlet Marina, NC','DUKN7 - Duck Pier, NC'};
 
-% lon=[77.95360000, -77.78670000, -76.67000000, -75.70420000, -75.54810000,
-% -75.74670000]
-% lat=[34.22750000, 34.21330000, 34.72000000, 35.20860000, 35.79500000,
-% 36.18330000]
+lon=[-77.95360000, -77.78670000, -76.67000000, -75.70420000, -75.54810000, -75.74670000]
+lat=[34.22750000, 34.21330000, 34.72000000, 35.20860000, 35.79500000,36.18330000]
 
+for i=1:length(stations)
+    plot(lon(i),lat(i), 'Marker','diamond','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
+    %text(lon(i)-0.02,lat(i)+.06, stations(i));
+end
+%annotation('textbox', [0.01, 0.1, 0.2, 0.4], 'String', station_names, 'BackgroundColor', 'w', 'EdgeColor', 'k', 'FontSize', 10);
+xlabel('Longitude (^oW)','fontsize',12);
+ylabel('Latitude (^oN)','fontsize',12);
+title('Albemarle-Pamlico Sound, North Carolina','fontsize',12);
+
+text(-78.0,34.32, 'WLON7');
+text(-77.706,34.213, 'JMPN7');
+text(-76.865,34.617, 'BFTN7');
+text(-75.804,35.10, 'HCGN7');
+text(-75.518,35.846, 'ORIN7');
+text(-75.70,36.184, 'DUKN7');
+grid('off');
+hold off
+exportgraphics(gcf,'NDBC_Station_Locations.png','Resolution',300)
+
+%{
 plot(-77.954,34.228, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
     text(-77.924,34.288, 'WLON7');
 plot(-77.786,34.213, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
@@ -67,51 +77,5 @@ plot(-75.548,35.796, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFa
 plot(-75.746,36.184, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
     text(-75.816,36.084, 'DUKN7');
 
-%{
-% NOAA NDBC
-%plot(-75.454,35.010, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k'); text(-75.424,35.010, '41025');
-%plot(-74.842,36.609, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');%text(-74.812,36.609, '44014');
-plot(-75.593,36.258, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-75.563,36.258, '44100');
-plot(-75.714,36.200, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-75.884,36.300, '44056');
-    
-plot(-75.421,36.001, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-75.391,36.001, '44086');
-plot(-75.330,35.750, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-75.300,35.750, '44095'); 
-plot(-75.454,35.010, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-75.424,35.010, '41025');
-plot(-76.949,34.213, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-77.019,34.303, '41159'); 
-plot(-76.949,34.207, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-77.059,34.137, '41064');  
-plot(-77.362,33.988, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-77.332,33.988, '41037');
-plot(-77.715,34.142, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-77.685,34.142, '41110');
-plot(-77.715,34.141, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-77.785,34.071, '41038'); 
-%plot(-77.764,33.441, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    %text(-77.734,33.441, '41013');
-%plot(-78.016,33.722, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    %text(-77.986,33.722, '41108');
-%plot(-78.477,33.837, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    %text(-78.447,33.837, '41024');
-    
-plot(-75.739,36.190, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-75.689,36.190, 'FRFN7');  
-
-plot(-76.525,34.622, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    text(-76.495,34.622, 'CLKN7');
-
-
 %}
-%%%%% These stations outside of model mesh area    
-%plot(-72.196,34.751, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    %text(-72.166,34.751, '41001');
-%plot(-74.945,31.760, 'Marker','o','MarkerSize',5,'MarkerEdgeColor','k','MarkerFaceColor','k');
-    %text(-74.915,31.760, '41002');
 
-hold off
-exportgraphics(gcf,'figures/NDBC_Station_Locations.png','Resolution',300)
