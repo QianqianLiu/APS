8ab16b0bfc20593ae0e805685ad53c130a590d90
Katherine Boot <kboot@login02.expanse.sdsc.edu>
2024-08-06 08:58:42 -0700

    update code test

---
+++ 00_in_progress/Validate_NRE.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+from pylib import *
+from sklearn.metrics import r2_score, mean_squared_error
+import numpy as np
+
+# coordinates of PS stations (ps1 to ps9)
+# Function to calculate RMSE
+def calculate_rmse(observed, modeled):
+    return np.sqrt(mean_squared_error(observed, modeled))
+
+# Function to calculate R-squared
+def calculate_r_squared(observed, modeled):
+    return r2_score(observed, modeled)
+
+# import stations and extract points from model outputs
+
+bpfile=('station_nre.bp')
+bp = read_schism_bpfile(bpfile)
+stations=[0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180]
+
+S=loadz('/home/liuquncw/APS/Obs/ModMon/NRE_WQ_2019.npz')
+mod=loadz('./mod_at_nre_stations.npz')
+model_run_name = 'RUN04d'
+model_run_descrip='2019 Hindcast Simulation'
+
+# make a sample plot
+figure(figsize=[16, 6])
+xts,xls=get_xtick(fmt=2,xts=[datenum(2019,1,1),datenum(2019,12,30)],str='%d/%b')
+xts,xls=xts[::60],xls[::60]; xls[0]=xls[0]+', 2018'
+
+mond = [datenum(2018,i+1,1) for i in range(23)]
+Mond=np.array(mond); mons=Mond*24*3600
+datetime= num2date(Mond)#+datenum(2018,1,1))
+Datestr=[datetime[i].strftime('%m') for i in range(23)]
+
+for i,sta in enumerate([20, 30, 50, 70,100,160]):#enumerate(stations[1:19:3]): #arange(0,190,10):
+    print(i)
+    #print(sta)
+    #figure(figsize=[16, 6])
+    subplot(3,2,i+1)
+    #figure(figsize=[8, 3.5])
+    pd=(S.station==sta)*(S.depthcat=="S")
+    pdm=stations.index(sta)
+    plot(S.time[pd],S.temp[pd],'r*', label='Observed')
+    plot(mod.time+datenum(2019,1,1),mod.temp[pdm,:],'b', label='Model')
+    
+    r2 = calculate_r_squared(S.temp[pd], mod.temp[pdm,:])
+    rmse = calculate_rmse(S.temp[pd], mod.temp[pdm,:])
+    
+    text(datenum(2019, 1, 1), 28, f'R² = {r2:.2f}\nRMSE = {rmse:.2f}', fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
+    
+    setp(gca(),xticks=Mond,xticklabels=[],xlim=[datenum(2019,1,1),datenum(2019,12,30)],ylim=[0,30])
+    if i>3: setp(gca(),xticks=Mond,xticklabels=Datestr,xlim=[datenum(2019,1,1),datenum(2019,12,30)],ylim=[0,30])
+    if i>3:
+        plt.xlabel('Month')
+    if i ==2:
+        plt.ylabel('Temperature (\u00b0C)')
+    xticks(rotation=70)
+    title('Station {}'.format(sta))
+    grid(linestyle = '--', linewidth = 0.5)
+    if i==1:
+        plt.legend()
+    else:
+        continue
+plt.suptitle('Temperature Comparison at NRE Stations - {}'.format(model_run_descrip))
+
+show(block=False)
+savefig('figures_validate/Compare_Temp_NRE_{}.png'.format(model_run_name))
+
+figure(figsize=[16, 6])
+xts,xls=get_xtick(fmt=2,xts=[datenum(2019,1,1),datenum(2019,12,30)],str='%d/%b')
+xts,xls=xts[::60],xls[::60]; xls[0]=xls[0]+', 2018'
+
+
+for i,sta in enumerate([20, 30, 50,70,100,160]):#enumerate(stations[1:19:3]): #arange(0,190,10):
+    print(i)
+    subplot(3,2,i+1)
+    pd=(S.station==sta)*(S.depthcat=="S")
+    pd2=(S.station==sta)*(S.depthcat=="B")
+    pdm=stations.index(sta)
+    plot(S.time[pd],S.salt[pd],'r*', label='Observed Surface')
+    plot(S.time[pd2],S.salt[pd2],'m*', label='Observed Bottom')
+    plot(mod.time+datenum(2019,1,1),mod.salt[pdm,:],'b', label='Model')
+    setp(gca(),xticks=Mond,xticklabels=[],xlim=[datenum(2019,1,1),datenum(2019,12,30)],ylim=[0,30])
+    if i>3: setp(gca(),xticks=Mond,xticklabels=Datestr,xlim=[datenum(2019,1,1),datenum(2019,12,30)],ylim=[0,30])
+    if i>3:
+        plt.xlabel('Month')
+    if i ==2:
+        plt.ylabel('Salinity (PSU)')
+    xticks(rotation=70)
+    title('Station {}'.format(sta))
+    grid(linestyle = '--', linewidth = 0.5)
+    if i==1:
+        plt.legend()
+    else:
+        continue
+plt.suptitle('Salinity Comparison at NRE Stations - {}'.format(model_run_descrip))
+
+show(block=False)
+savefig('figures_validate/Compare_Salt_NRE_{}.png'.format(model_run_name))
+
+
