d3088e0b37fade06dd1d132bfb5dfaecc4ad6f6a
kboot2022 <kboot42@gmail.com>
2024-09-04 10:51:36 -0400

    figure updates validation

--- 00_in_progress/Validate_NRE.py
+++ 00_in_progress/Validate_NRE.py
@@ -3,51 +3,73 @@ from pylib import *
 from sklearn.metrics import r2_score, mean_squared_error
 import numpy as np
 
-# coordinates of PS stations (ps1 to ps9)
-# Function to calculate RMSE
-def calculate_rmse(observed, modeled):
-    return np.sqrt(mean_squared_error(observed, modeled))
-
-# Function to calculate R-squared
-def calculate_r_squared(observed, modeled):
-    return r2_score(observed, modeled)
-
 # import stations and extract points from model outputs
 
-bpfile=('station_nre.bp')
+bpfile='station_nre.bp'
 bp = read_schism_bpfile(bpfile)
 stations=[0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180]
 
 S=loadz('/home/liuquncw/APS/Obs/ModMon/NRE_WQ_2019.npz')
-mod=loadz('./mod_at_nre_stations.npz')
+#mod=loadz('./mod_at_nre_stations.npz')
+mod = loadz('/expanse/lustre/projects/unc107/kboot/ModelResults/RUN04d/mod_at_nre_stations.npz')
 model_run_name = 'RUN04d'
 model_run_descrip='2019 Hindcast Simulation'
+
+# Convert timesteps to datestr
+mond = [datenum(2019, i + 1, 1) for i in range(23)]
+Mond = np.array(mond); mons = Mond * 24 * 3600
+datetime = num2date(Mond)
+Datestr = [datetime[i].strftime('%m') for i in range(23)]
+
+# Functions for R-value and RMSE
+# Initialize value arrays for all stations
+r_df = np.full(len(stations), np.nan)
+rmse_df = np.full(len(stations), np.nan)
+#r_sq_df = np.full(len(stations), np.nan)
+
+def calc_r_value(observed, modeled):
+    # Calculate the correlation matrix
+    corr_matrix = np.corrcoef(observed, modeled)
+    # Extract the correlation coefficient
+    return corr_matrix[0, 1]
+
+def calc_rmse_value(observed, modeled):
+    mse = mean_squared_error(observed, modeled)
+    rmse = np.sqrt(mse) # root of mse
+    return rmse
 
 # make a sample plot
 figure(figsize=[16, 6])
 xts,xls=get_xtick(fmt=2,xts=[datenum(2019,1,1),datenum(2019,12,30)],str='%d/%b')
 xts,xls=xts[::60],xls[::60]; xls[0]=xls[0]+', 2018'
 
-mond = [datenum(2018,i+1,1) for i in range(23)]
-Mond=np.array(mond); mons=Mond*24*3600
-datetime= num2date(Mond)#+datenum(2018,1,1))
-Datestr=[datetime[i].strftime('%m') for i in range(23)]
-
-for i,sta in enumerate([20, 30, 50, 70,100,160]):#enumerate(stations[1:19:3]): #arange(0,190,10):
+# Select and plot NRE stations with R and RMSE values in each subplot
+for i,sta in enumerate([20, 30, 50, 70,100,160]):
     print(i)
-    #print(sta)
-    #figure(figsize=[16, 6])
+    pd = (S.station == sta) & (S.depthcat == "S") & (S.time >= datenum(2019,1,1)) & (S.time < datenum(2020,1,1))
+    pdm = stations.index(sta)
+
+    # make subplots
     subplot(3,2,i+1)
-    #figure(figsize=[8, 3.5])
-    pd=(S.station==sta)*(S.depthcat=="S")
-    pdm=stations.index(sta)
     plot(S.time[pd],S.temp[pd],'r*', label='Observed')
     plot(mod.time+datenum(2019,1,1),mod.temp[pdm,:],'b', label='Model')
     
-    r2 = calculate_r_squared(S.temp[pd], mod.temp[pdm,:])
-    rmse = calculate_rmse(S.temp[pd], mod.temp[pdm,:])
+    # Extract model points at same timesteps as observed
+    # take observation time and limit to only 2019
+    obs_times = S.time[pd] # this is a datenum
+    mod_times = mod.time + datenum(2019, 1, 1)
+    mod_temps_at_obs_times = np.interp(obs_times, mod_times, mod.temp[pdm, :])
     
-    text(datenum(2019, 1, 1), 28, f'R² = {r2:.2f}\nRMSE = {rmse:.2f}', fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
+    # calculate R-values and RMSE
+    r_val = calc_r_value(S.temp[pd], mod_temps_at_obs_times).round(3)
+    rmse_val = calc_rmse_value(S.temp[pd], mod_temps_at_obs_times).round(2)
+    r_df[pdm] = r_val
+    rmse_df[pdm] = rmse_val
+
+    #print(f"station {sta}, Temp R-Squared: {r_sq}")
+    print(f"station {sta}, Temp R-value: {r_val}, Temp RMSE: {rmse_val}")
+
+    text(datenum(2019, 7, 15), 10, f'R = {r_val:.2f}\nRMSE = {rmse_val:.2f}', fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
     
     setp(gca(),xticks=Mond,xticklabels=[],xlim=[datenum(2019,1,1),datenum(2019,12,30)],ylim=[0,30])
     if i>3: setp(gca(),xticks=Mond,xticklabels=Datestr,xlim=[datenum(2019,1,1),datenum(2019,12,30)],ylim=[0,30])
@@ -63,24 +85,45 @@ for i,sta in enumerate([20, 30, 50, 70,100,160]):#enumerate(stations[1:19:3]): #
     else:
         continue
 plt.suptitle('Temperature Comparison at NRE Stations - {}'.format(model_run_descrip))
-
 show(block=False)
 savefig('figures_validate/Compare_Temp_NRE_{}.png'.format(model_run_name))
 
+#scp -r kboot@login.expanse.sdsc.edu:/expanse/lustre/projects/unc107/kboot/ModelResults/RUN04d/figures_validate/*.png .
+# Print r-squared values for all stations
+print("Final RMSE values for all stations:")
+print(r_df)
+print("Final R-values for all stations:")
+print(rmse_df)
+
+# Next figure for Salinity
 figure(figsize=[16, 6])
 xts,xls=get_xtick(fmt=2,xts=[datenum(2019,1,1),datenum(2019,12,30)],str='%d/%b')
 xts,xls=xts[::60],xls[::60]; xls[0]=xls[0]+', 2018'
 
 
-for i,sta in enumerate([20, 30, 50,70,100,160]):#enumerate(stations[1:19:3]): #arange(0,190,10):
+for i,sta in enumerate([20, 30, 50,70,100,160]):
     print(i)
     subplot(3,2,i+1)
-    pd=(S.station==sta)*(S.depthcat=="S")
-    pd2=(S.station==sta)*(S.depthcat=="B")
+    pd = (S.station == sta) & (S.depthcat == "S") & (S.time >= datenum(2019,1,1)) & (S.time < datenum(2020,1,1))
+    pd2 = (S.station == sta) & (S.depthcat == "B") & (S.time >= datenum(2019,1,1)) & (S.time < datenum(2020,1,1))
     pdm=stations.index(sta)
+    
     plot(S.time[pd],S.salt[pd],'r*', label='Observed Surface')
     plot(S.time[pd2],S.salt[pd2],'m*', label='Observed Bottom')
-    plot(mod.time+datenum(2019,1,1),mod.salt[pdm,:],'b', label='Model')
+    plot(mod.time+datenum(2019,1,1),mod.salt[pdm,:],'b', label='Model Surface?')
+
+    obs_times = S.time[pd] # this is a datenum
+    mod_times = mod.time + datenum(2019, 1, 1)
+    mod_salt_at_obs_times = np.interp(obs_times, mod_times, mod.salt[pdm, :])
+    
+    # calculate R-values and RMSE
+    r_val = calc_r_value(S.salt[pd], mod_salt_at_obs_times).round(3)
+    rmse_val = calc_rmse_value(S.salt[pd], mod_salt_at_obs_times).round(2)
+    r_df[pdm] = r_val
+    rmse_df[pdm] = rmse_val
+    print(f"station {sta}, Temp R-value: {r_val}, Temp RMSE: {rmse_val}")
+    text(datenum(2019, 2, 1), 20, f'R = {r_val:.2f}\nRMSE = {rmse_val:.2f}', fontsize=10, bbox=dict(facecolor='white', alpha=0.5))
+
     setp(gca(),xticks=Mond,xticklabels=[],xlim=[datenum(2019,1,1),datenum(2019,12,30)],ylim=[0,30])
     if i>3: setp(gca(),xticks=Mond,xticklabels=Datestr,xlim=[datenum(2019,1,1),datenum(2019,12,30)],ylim=[0,30])
     if i>3:
@@ -95,7 +138,6 @@ for i,sta in enumerate([20, 30, 50,70,100,160]):#enumerate(stations[1:19:3]): #a
     else:
         continue
 plt.suptitle('Salinity Comparison at NRE Stations - {}'.format(model_run_descrip))
-
 show(block=False)
 savefig('figures_validate/Compare_Salt_NRE_{}.png'.format(model_run_name))
 
